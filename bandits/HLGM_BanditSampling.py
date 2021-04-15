9931f1eea94a0450dfc36e0fcd5c48c94f68ee24
Varshant Dhar <varshant@icloud.com>
2021-04-15 14:59:56 -0700

    Rotation Axes

--- bandits/HLGM_BanditSampling.py
+++ bandits/HLGM_BanditSampling.py
@@ -20,9 +20,12 @@ def action_segments():
 	env = deepmind_lab.Lab('tests/empty_room_test', [])
 	action_spec = env.action_spec()
 	x = 512
-	coords = [x]
+	coords = [[x,y]]
 	theta = [(t * np.pi / 180) for t in range(45, 360, 45)]
-	coords.append([np.floor(x*np.sin(t)) for t in theta])
+	for t in theta:
+		new_x = np.floor(x*np.cos(t) - y*np.sin(t))
+		new_y = np.floor(x*np.sin(t) + y*np.cos(t))
+		coords.append([new_x, new_y])
 	pprint(coords)
 
 if __name__ == '__main__':
