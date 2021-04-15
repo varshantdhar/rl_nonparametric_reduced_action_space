6effa6b7e5da82d282e935976f83572b2165507f
Varshant Dhar <varshant@icloud.com>
2021-04-15 14:47:30 -0700

    Changes

--- bandits/HLGM_BanditSampling.py
+++ bandits/HLGM_BanditSampling.py
@@ -5,22 +5,28 @@ import numpy as np
 import scipy.stats as stats
 import pickle
 import sys, os
-from itertools import *
 import pdb
-from matplotlib import colors
-from pprintpp import pprint
-import inspect
+
+import deepmind_lab
 
 from MCMCBanditSampling import *
-import deepmind_lab
+from itertools import *
+from matplotlib import colors
+from pprintpp import pprint
 
 
 def action_segments():
 	env = deepmind_lab.Lab('tests/empty_room_test', [])
 	action_spec = env.action_spec()
-	action_index = {action['name']: i for i, action in enumerate(action_spec)}
-	pprint(action_index)
-
+	x = 512
+	y = -512
+	coords = [[x,y]]
+	theta = [(t * np.pi / 180) for t in range(45, 360, 45)]
+	for t in theta:
+		new_x = np.ceil(x*np.cos(t) - y*np.sin(t))
+		new_y = np.ceil(x*np.sin(t) + y*np.cos(t))
+		coords.append([new_x, new_y])
+	pprint(coords)
 
 if __name__ == '__main__':
 	path = os.path.dirname(inspect.getfile(deepmind_lab))
