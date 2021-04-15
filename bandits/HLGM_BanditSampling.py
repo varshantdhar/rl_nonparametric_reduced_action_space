15fc683416da75eab0c419cf20f2253fe9b45c64
Varshant Dhar <varshant@icloud.com>
2021-04-15 14:24:36 -0700

    Changes

--- bandits/HLGM_BanditSampling.py
+++ bandits/HLGM_BanditSampling.py
@@ -15,16 +15,17 @@ from MCMCBanditSampling import *
 import deepmind_lab
 
 
-def run():
+def action_segments():
 	env = deepmind_lab.Lab('tests/empty_room_test', [])
 	action_spec = env.action_spec()
-	pprint(action_spec)
+	action_index = {action['name']: i for i, action in enumerate(action_spec)}
+	pprint(action_index)
 
 
 if __name__ == '__main__':
 	path = os.path.dirname(inspect.getfile(deepmind_lab))
 	deepmind_lab.set_runfiles_path(path)
-	run()
+	action_segments()
 
 	# deepmind_lab.set_runfiles_path(runfiles_path)
 
