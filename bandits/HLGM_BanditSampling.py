4768c4691708ca12c4851d77d0328e0380c22563
Varshant Dhar <varshant@icloud.com>
2021-04-19 10:11:18 -0700

    Testing seekavoid_arena_01

--- bandits/HLGM_BanditSampling.py
+++ bandits/HLGM_BanditSampling.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
 
     coords, A = agent.action_segments()  # Rotation Axes, Number of Arms
     rewards = 0
-    R = 100 # Number of realizations to run
+    R = 20 # Number of realizations to run
     t_max = 1000 # Time-instants to run the bandit
     width = 8
     height = 8
