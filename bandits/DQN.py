3c2fd3f5d0bfe3e4025f14c3dcc643d0e3b2f1d0
Varshant Dhar <varshant@icloud.com>
2021-04-20 07:24:22 -0700

    Changed replay buffer configuration from action_indices to action

--- bandits/DQN.py
+++ bandits/DQN.py
@@ -21,13 +21,13 @@ def ql(env, agent, context, frame_count, running_rewards):
     if not env.is_running():
         running_rewards = 0
         done = True
-        agent.replay_buffer.add_sample(state, action_ind, reward, state, done)
+        agent.replay_buffer.add_sample(state, action, reward, state, done)
         agent.train_step(frame_count)
         return (reward, running_rewards)
     next_context = env.observations()["RGB_INTERLEAVED"].flatten()
     next_state = agent.get_state(torch.Tensor(next_context))
         
-    agent.replay_buffer.add_sample(state, action_ind, reward, next_state, done)
+    agent.replay_buffer.add_sample(state, action, reward, next_state, done)
     agent.train_step(frame_count)
     if reward > 0:
         running_rewards += reward
