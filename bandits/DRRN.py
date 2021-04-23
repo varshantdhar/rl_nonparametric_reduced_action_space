206b2294c0087e274597cf39897a63abfeec6906
Varshant Dhar <varshant@icloud.com>
2021-04-23 11:38:25 -0700

    HLGM-DRRN Test Simulation: Debugging

--- bandits/DRRN.py
+++ bandits/DRRN.py
@@ -187,7 +187,7 @@ class DRRN_Agent:
         self.batch_size = 64
         self.action_dim = 1024
         self.obs_dim = 256
-        self.network = DRRN(self.obs_dim, embedding_dim=128, hidden_dim=128)
+        self.network = DRRN(self.action_dim, self.obs_dim, embedding_dim=128, hidden_dim=128)
         self.memory = ReplayMemory(capacity=5000)
         self.save_path = 'logs'
         self.clip = 5
